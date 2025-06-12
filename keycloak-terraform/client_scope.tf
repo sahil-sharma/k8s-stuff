02cdb2c75190478185d9d03fa05b28df34fa02be
Sahil Sharma <sahil.sharma@getbyrd.com>
2025-06-12 19:11:01 +0200

    Add Realm and Client Role Mappers Directly to the Client

--- keycloak-terraform/client_scope.tf
+++ keycloak-terraform/client_scope.tf
@@ -17,57 +17,67 @@ resource "keycloak_openid_group_membership_protocol_mapper" "groups_mapper" {
   add_to_userinfo     = true
 }
 
-# resource "keycloak_openid_client_scope" "client_roles_scope" {
-#   realm_id               = keycloak_realm.realm.id
-#   name                   = "client-roles"
-#   description            = "Add user client roles to ID token"
-#   include_in_token_scope = true
-#   gui_order              = 2
-# }
-
-# resource "keycloak_generic_protocol_mapper" "roles_mapper" {
-#   realm_id        = keycloak_realm.realm.id
-#   client_scope_id = keycloak_openid_client_scope.client_roles_scope.id
-#   name            = "roles-mapper"
-#   protocol        = "openid-connect"
-#   protocol_mapper = "oidc-usermodel-client-role-mapper"
-#   config = {
-#     "claim.name"           = "roles"
-#     "jsonType.label"       = "String"
-#     "multivalued"          = "true"
-#     "userinfo.token.claim" = "true"
-#     "id.token.claim"       = "true"
-#     "access.token.claim"   = "true"
-#     "aggregate.attrs"      = "false"
-#   }
-# }
-
-resource "keycloak_openid_client_default_scopes" "default_scopes" {
-  for_each = { for client in var.clients : client.client_id => client }
-
-  realm_id  = keycloak_realm.realm.id
-  client_id = keycloak_openid_client.clients[each.key].id
-  default_scopes = [
-    "profile",
-    "email",
-    "roles",
-    keycloak_openid_client_scope.groups.name,
-    #keycloak_openid_client_scope.client_roles_scope.name
-  ]
+resource "keycloak_openid_client_scope" "client_roles_scope" {
+  realm_id               = keycloak_realm.realm.id
+  name                   = "client-roles"
+  description            = "Add user client roles to ID token"
+  include_in_token_scope = true
+  gui_order              = 2
 }
 
-resource "keycloak_generic_protocol_mapper" "audience_mapper" {
+resource "keycloak_generic_protocol_mapper" "roles_mapper" {
+  realm_id        = keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.client_roles_scope.id
+  name            = "roles-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usermodel-client-role-mapper"
+  config = {
+    "claim.name"           = "roles"
+    "jsonType.label"       = "String"
+    "multivalued"          = "true"
+    "userinfo.token.claim" = "true"
+    "id.token.claim"       = "true"
+    "access.token.claim"   = "true"
+    "aggregate.attrs"      = "false"
+  }
+}
+
+# Add realm roles mapper to the client
+resource "keycloak_generic_protocol_mapper" "realm_roles_mapper" {
   for_each = { for client in var.clients : client.client_id => client }
 
   realm_id        = keycloak_realm.realm.id
   client_id       = keycloak_openid_client.clients[each.key].id
-  name            = "audience-mapper"
+  name            = "realm-roles"
   protocol        = "openid-connect"
-  protocol_mapper = "oidc-audience-mapper"
+  protocol_mapper = "oidc-usermodel-realm-role-mapper"
   config = {
-    "included.client.audience" = each.key
-    "id.token.claim"           = "true"
-    "access.token.claim"       = "true"
-    "userinfo.token.claim"     = "false"
+    "claim.name"           = "realm_access.roles"
+    "jsonType.label"       = "String"
+    "multivalued"          = "true"
+    "userinfo.token.claim" = "true"
+    "id.token.claim"       = "true"
+    "access.token.claim"   = "true"
+    "aggregate.attrs"      = "false"
+  }
+}
+
+# Client roles mapper
+resource "keycloak_generic_protocol_mapper" "client_roles_mapper" {
+  for_each = { for client in var.clients : client.client_id => client }
+
+  realm_id        = keycloak_realm.realm.id
+  client_id       = keycloak_openid_client.clients[each.key].id
+  name            = "client-roles"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usermodel-client-role-mapper"
+  config = {
+    "claim.name"           = "resource_access.${each.key}.roles"
+    "jsonType.label"       = "String"
+    "multivalued"          = "true"
+    "userinfo.token.claim" = "true"
+    "id.token.claim"       = "true"
+    "access.token.claim"   = "true"
+    "aggregate.attrs"      = "false"
   }
 }
