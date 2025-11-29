f513c69e43143fc8e137fd6e2ef4aa15c7dc527c
Sahil Sharma <sahil.sharma@protonmail.com>
2025-11-29 15:16:14 +0100

    Modify test.sh script and add readme file

--- kafka-test-client/test.sh
+++ kafka-test-client/test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -e
 
+PROPERTIES_FILE="/opt/client.properties"
+TOPIC_NAME="my-kafka-topic"
+KAFKA_BOOTSTRAP_SERVER="my-cluster-kafka-bootstrap.kafka:9093"
+
 # Produce messages to Kafka topic
 bash $KAFKA_HOME/bin/kafka-console-producer.sh \
-  --topic my-kafka-topic \
-  --bootstrap-server my-cluster-kafka-bootstrap.kafka:9093 \
-  --producer.config /opt/client.properties
+  --topic $TOPIC_NAME \
+  --bootstrap-server $KAFKA_BOOTSTRAP_SERVER \
+  --producer.config $PROPERTIES_FILE
 
 # Consume messages from Kafka topic
 bash $KAFKA_HOME/bin/kafka-console-consumer.sh \
-  --bootstrap-server my-cluster-kafka-bootstrap.kafka:9093 \
-  --topic my-kafka-topic \
+  --bootstrap-server $KAFKA_BOOTSTRAP_SERVER     \
+  --topic $TOPIC_NAME \
   --from-beginning \
-  --consumer.config /opt/client.properties
+  --consumer.config $PROPERTIES_FILE
