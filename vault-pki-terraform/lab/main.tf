d8146251e05ae05cea60bf943c680be62dc0c5b9
Sahil Sharma <sahil.sharma@protonmail.com>
2026-05-22 11:55:31 +0200

    Update App PG values

--- vault-pki-terraform/lab/main.tf
+++ vault-pki-terraform/lab/main.tf
@@ -1,11 +1,11 @@
 module "k8s_auth_method" {
-  source          = "../../modules/vault-k8s-auth-method"
+  source          = "../modules/vault-k8s-auth-method"
   path            = var.kubernetes_auth_path
   kubernetes_host = var.kubernetes_host
 }
 
 module "pki" {
-  source = "../../modules/vault-pki"
+  source = "../modules/vault-pki"
 
   name = var.pki_name
 
@@ -23,12 +23,12 @@ module "pki" {
 }
 
 module "k8s_auth" {
-  source   = "../../modules/vault-k8s-auth-binding"
+  source   = "../modules/vault-k8s-auth-binding"
   for_each = var.k8s_auth_bindings
 
   name                             = each.key
   policy_name                      = each.value.policy_name
-  vault_role_name                  = each.value.vault_role_name 
+  vault_role_name                  = each.value.vault_role_name
   kubernetes_auth_path             = module.k8s_auth_method.path
   bound_service_account_names      = each.value.bound_service_account_names
   bound_service_account_namespaces = each.value.bound_service_account_namespaces
